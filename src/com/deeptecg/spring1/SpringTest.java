80a5fa321620e4fb4f86012062ffad553d01418f
fatoosh <aftabkhadri905@gmail.com>
2023-11-07 18:04:56 +0530

    7-11

---
+++ src/com/deeptecg/spring1/SpringTest.java
@@ -0,0 +1,16 @@
+package com.deeptecg.spring1;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+public class SpringTest {
+
+	
+	public static void main(String[] args) {
+		Resource res = new ClassPathResource("ApplicationContext.xml"); //taking class name and passing bean id
+		BeanFactory ft = new XmlBeanFactory(res);
+		Student s = (Student) ft.getBean("stu");
+		s.display();
+	}
+
+}
---
+++ src/com/deeptecg/spring1/Student.java
@@ -0,0 +1,26 @@
+package com.deeptecg.spring1;
+
+public class Student {
+
+	private int student_id;
+	private String student_name;
+	
+	//getters setters
+	public int getStudent_id() {
+		return student_id;
+	}
+	public void setStudent_id(int student_id) {
+		this.student_id = student_id;
+	}
+	public String getStudent_name() {
+		return student_name;
+	}
+	public void setStudent_name(String student_name) {
+		this.student_name = student_name;
+	}
+	
+	void display() {
+		System.out.println(""+student_id);
+		System.out.println(""+student_name);
+	}
+}
